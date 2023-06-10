b3fa0cb36a7634838a5bcaf9bae255ec82248c12
jayeshshaw <jayesh.shaw10@gmail.com>
2023-06-11 00:26:12 +0530

    Added check for name,email & password

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const {check, validationResult} = require('express-validator');
 
 
-//@route GET api/users
-//@desc Test Route
+
+//@route POST api/users
+//@desc Register User
 //@access Public
 
-router.get('/', (req,res) => res.send('User Route...'));
+router.post('/',
+
+[
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Include a valid email').isEmail(),
+    check('password', 'Password should be atleast 6 characters long').isLength({min: 6})
+
+], 
+
+(req,res) => {
+    // console.log(req.body);
+    
+    const errors= validationResult(req);
+    if(!errors.isEmpty()){ return res.status(400).json({errors: errors.array()})}
+
+
+    res.send('User Route...')
+});
 
 
 module.exports= router;
--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app=express();
 connectDB();
 
 
+//Initn Middleware
+app.use(express.json({ extended: false }));
+
+
 app.get('/', (req,res) => res.send('API Running...'));
 
 
