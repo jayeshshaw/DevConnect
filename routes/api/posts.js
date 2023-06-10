3fa0095b806422d373e0461801a0c8a2d7a41fdf
jayeshshaw <jayesh.shaw10@gmail.com>
2023-06-10 22:47:19 +0530

    connected DB and added Routes

---
+++ routes/api/posts.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+
+//@route GET api/users
+//@desc Test Route
+//@access Public
+
+router.get('/', (req,res) => res.send('Posts Route...'));
+
+
+module.exports= router;
