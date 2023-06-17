fc175a5568e0ba1ce0a05251c3adf23dcbb2b008
jayeshshaw <jayesh.shaw10@gmail.com>
2023-06-17 15:18:25 +0530

    implemented middleware to verify jwt

---
+++ middleware/auth.js
@@ -0,0 +1,26 @@
+const jwt= require('jsonwebtoken');
+const config= require('config');
+
+module.exports= function (res,req,next){
+
+    //Get token from header
+    const token = req.header('x-auth-token');
+
+    //If token is NULL
+    if(!token){
+        return res.status(401).json( { msg: 'No token found, authorization denied' } );
+    }
+
+    //Verify Token
+    try{
+        const decoded= jwt.verify(token, config.get('jwtToken'));
+
+        req.user = decoded.user;
+        next();
+    }
+    catch ( err ) {
+        res.status(401).json( { msg: 'Token is not valid' } );
+    }
+
+
+}
