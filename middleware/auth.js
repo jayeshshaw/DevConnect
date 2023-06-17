05ed79c34a9469a6503455f59e21845683b015b9
jayeshshaw <jayesh.shaw10@gmail.com>
2023-06-17 15:26:09 +0530

    updated  middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,26 +1,27 @@
-const jwt= require('jsonwebtoken');
-const config= require('config');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
-module.exports= function (res,req,next){
+module.exports = function (req, res, next) {
+  // Get token from header
+  const token = req.header('x-auth-token');
 
-    //Get token from header
-    const token = req.header('x-auth-token');
+  // Check if not token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
 
-    //If token is NULL
-    if(!token){
-        return res.status(401).json( { msg: 'No token found, authorization denied' } );
-    }
-
-    //Verify Token
-    try{
-        const decoded= jwt.verify(token, config.get('jwtToken'));
-
+  // Verify token
+  try {
+    jwt.verify(token, config.get('jwtToken'), (error, decoded) => {
+      if (error) {
+        return res.status(401).json({ msg: 'Token is not valid' });
+      } else {
         req.user = decoded.user;
         next();
-    }
-    catch ( err ) {
-       return res.status(401).json( { msg: 'Token is not valid' } );
-    }
-
-
-}
+      }
+    });
+  } catch (err) {
+    console.error('something wrong with auth middleware');
+    res.status(500).json({ msg: 'Server Error' });
+  }
+};
