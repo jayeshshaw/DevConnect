a0ae70f88acdc97c2b06a12f67ca486de54c7b1a
jayeshshaw <jayesh.shaw10@gmail.com>
2023-06-17 15:38:45 +0530

    return user data from jwt

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,15 +2,27 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 
+const User= require('../../models/User')
 
 
 //@route GET api/users
 //@desc Test Route
 //@access Public
 
-router.get('/', auth,  (req,res) => {
+router.get('/', auth, async (req,res) => {
 
-    
+    try{
+
+        const user= await User.findById(req.user.id).select('-password');
+        res.json(user);
+
+    }
+    catch(err){
+
+        console.error(err.message);
+        res.status(500).send('Server Error');
+
+    }
 
     res.send('Auth Route...')
 
