7f9308ea73b71bfcac3e98c78edfa7db60990b97
jayeshshaw <jayesh.shaw10@gmail.com>
2023-07-02 00:46:46 +0530

    getting profile for logges in user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const auth= require('../../middleware/auth')
+const {check,validationResult}= require('express-validator')
+
+const Profile= require('../../models/Profile');
+const User= require('../../models/User');
 
 
-//@route GET api/users
-//@desc Test Route
-//@access Public
+//@route GET api/profile/me
+//@desc Get current users profile
+//@access Private
 
-router.get('/', (req,res) => res.send('Profile Route...'));
+router.get('/me', auth, async (req,res) => {
+    try{
+        const profile= await Profile.findOne({user: req.user.id}).populate('user',
+        ['name','avatar']);
+
+        if(!profile){
+            return res.status(400).json({msg : 'There is no profile for this user'});
+        }
+
+        res.json(profile);
+    }
+    catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+
+//@route GET api/profile/me
+//@desc Get current users profile
+//@access Private
+
+
+
 
 
 module.exports= router;
