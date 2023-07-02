8dbd85610eb26d168f45fc81b46a2b66df296e6e
jayeshshaw <jayesh.shaw10@gmail.com>
2023-07-02 13:42:35 +0530

    updated mongoose strictQuery

--- config/db.js
+++ config/db.js
@@ -4,6 +4,7 @@ const db=config.get('mongoURI');
 
 const connectDB= async()=>{
     try{
+        mongoose.set('strictQuery', true);
         await mongoose.connect(db,{
             useNewUrlParser: true
         });
